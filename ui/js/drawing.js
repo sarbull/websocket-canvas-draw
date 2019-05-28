02e8b623d03c79819dda9f2628b85a1f91049975
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2019-05-28 15:39:33 +0300

    Prepend and add date time

--- ui/js/drawing.js
+++ ui/js/drawing.js
@@ -495,13 +495,23 @@ DrawingPad = function (options) {
       return setConfirmShare(data);
     });
 
+    function formatDate(template, date) {
+      var specs = 'YYYY:MM:DD:HH:mm:ss'.split(':');
+      date = new Date(date || Date.now() - new Date().getTimezoneOffset() * 6e4);
+      return date.toISOString().split(/[-:.TZ]/).reduce(function(template, item, i) {
+        return template.split(specs[i]).join(item);
+      }, template);
+    }
+
+
+
     DP.thisObj.socket.on('message', function (data) {
       let list = document.querySelector('#list');
       let el = document.createElement('li');
 
-      el.innerHTML = `<strong>Dunno</strong>: ${data.message}`;
+      el.innerHTML = `<strong>Dunno</strong> (${formatDate(new Date())}): ${data.message}`;
 
-      list.append(el);
+      list.prepend(el);
     });
 
     //set event handlers
