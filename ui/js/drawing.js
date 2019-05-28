4b95cae4396b872e4d4b0e849cac2377052fbea0
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2019-05-28 15:40:46 +0300

    fix

--- ui/js/drawing.js
+++ ui/js/drawing.js
@@ -499,7 +499,7 @@ DrawingPad = function (options) {
       var specs = 'YYYY:MM:DD:HH:mm:ss'.split(':');
       date = new Date(date || Date.now() - new Date().getTimezoneOffset() * 6e4);
       return date.toISOString().split(/[-:.TZ]/).reduce(function(template, item, i) {
-        return template.split(specs[i]).join(item);
+        return template.toString().split(specs[i]).join(item);
       }, template);
     }
 
